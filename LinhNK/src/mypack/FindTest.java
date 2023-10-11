8c01652c03ca02cc20fde695167d4382165613c5
P52 <P52@Lynko>
2023-10-11 22:40:16 +0700

    1

---
+++ LinhNK/src/mypack/FindTest.java
@@ -0,0 +1,13 @@
+package mypack;
+
+public class FindTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+CommonBrowser cb = new CommonBrowser ();
+	WebDriver driver = cb.initChromeDriver ("https://bepantoan.vn");
+	cb.pause (3000);
+	
+}
